cc5ebedf5388194afe4d586ff51d841131768288
anagalit <ana.galit1@gmail.com>
2022-11-21 16:55:24 +0100

    Add unit and integration tests for GET method, add the spring.mongodb.embedded.version 4.0.21 to the application.properties file

---
+++ backend/src/test/java/shortify/backend/LinkIntegrationTest.java
@@ -0,0 +1,28 @@
+package shortify.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LinkIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @DirtiesContext
+    @Test
+    void getLinksAndExpectEmptyListOfLinks() throws Exception {
+        
+        mvc.perform(MockMvcRequestBuilders.get("/api/links"))
+                .andExpect(status().isOk())
+                .andExpect((content().json("[]")));
+    }
+}
