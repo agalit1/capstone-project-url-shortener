01d24367f45c93533efe6cb03d825d2355c0e101
anagalit <ana.galit1@gmail.com>
2022-11-22 23:28:17 +0100

    Add new component in frontend (GetLink)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,17 @@
-import React, {useEffect, useState} from 'react';
 import './App.css';
-import axios from "axios";
-import {LinkModel} from "./LinkModel";
+import GetLink from "./GetLink";
+import {Route, BrowserRouter, Routes} from "react-router-dom";
 
 function App() {
 
-    const [links, setLinks] = useState<LinkModel[]>([]);
-
-    useEffect(() => {
-        fetchAllLinks()
-    }, [])
-
-    const fetchAllLinks = () => {
-        axios.get("/api/links")
-            .then(response => response.data)
-            .catch((error) => console.log("Endpoint not available " + error))
-            .then((data) => {
-                setLinks(data)
-            })
-    }
-
-    const linkList = links.map((current) =>
-        <li>{current.link}</li>
+    return (
+        <div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<GetLink/>}/>
+            </Routes>
+        </BrowserRouter>
+    </div>
     )
-
-    return <>
-        <h1>List of links:</h1>
-        <ul>{linkList}</ul>
-    </>
 }
 export default App;
