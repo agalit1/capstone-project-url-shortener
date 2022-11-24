d355cfee833aa218c46edc0b15359c8b1e91596b
anagalit <ana.galit1@gmail.com>
2022-11-24 09:25:02 +0100

    Delete PostLink component and integrate its functionality into LinkCard component, adjust the App.tsx file accordingly, add App.css and LinkCard.css

--- frontend/src/LinkCard.tsx
+++ frontend/src/LinkCard.tsx
@@ -1,10 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import {LinkModel} from "./LinkModel";
 import axios from "axios";
+import './Styling/LinkCard.css';
 
 function LinkCard() {
 
-    const [links, setLinks] = useState<LinkModel[]>([]);
+    const [getLinks, setGetLinks] = useState<LinkModel[]>([]);
+    const [postLongLink, setPostLongLink] = useState<string>('');
 
     useEffect(() => {
         fetchAllLinks()
@@ -13,18 +15,34 @@ function LinkCard() {
     const fetchAllLinks = () => {
         axios.get("/api/links")
             .then((response) => {
-                setLinks(response.data);
+                setGetLinks(response.data);
             })
             .catch((error) => console.log("Endpoint not available " + error))
     }
 
-    const linkList = links.map((current) =>
+    const linkList = getLinks.map((current) =>
         <li>{current.link}</li>
     )
 
+    const postLink = (event: ChangeEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        axios.post("/api/links", {
+            link: postLongLink,
+        })
+            .catch((error) => {
+                console.log("Endpoint not available " + error)
+            })
+    }
+
     return <>
-        <h4>MyUrls:</h4>
-        <ul>{linkList}</ul>
-    </>
+            <form onSubmit={postLink}>
+                <label htmlFor="Paste your long url">Enter your long url</label>
+                <input type="text" id={"Paste your long url"} onChange={(e) => setPostLongLink(e.target.value)}
+                       value={postLongLink}/>
+                <button>Click</button>
+                <h4>MyUrls:</h4>
+                <ul>{linkList}</ul>
+            </form>
+        </>
 }
 export default LinkCard;
