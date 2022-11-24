8d899696c6ce9277db41bc1d5609d4d1d798c530
anagalit <ana.galit1@gmail.com>
2022-11-24 09:25:02 +0100

    Delete PostLink component and integrate its functionality into LinkCard component, adjust the App.tsx file accordingly, add App.css and LinkCard.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
-import './App.css';
+import './Styling/App.css';
 import LinkCard from "./LinkCard";
 import {Route, BrowserRouter, Routes} from "react-router-dom";
-import PostLink from "./PostLink";
 
 function App() {
 
@@ -9,7 +8,7 @@ function App() {
         <div>
             <BrowserRouter>
                 <Routes>
-                    <Route path='/' element={<><PostLink/> <LinkCard/></>}/>
+                    <Route path='/' element={<LinkCard/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
