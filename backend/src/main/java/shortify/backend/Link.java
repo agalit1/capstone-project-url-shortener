9077a58eb5749d8621a102b42e3b77ad3874bd66
anagalit <ana.galit1@gmail.com>
2022-11-25 12:37:47 +0100

    Add validation for urls in backend

--- backend/src/main/java/shortify/backend/Link.java
+++ backend/src/main/java/shortify/backend/Link.java
@@ -1,9 +1,12 @@
 package shortify.backend;
 
+import org.hibernate.validator.constraints.URL;
+
 import javax.validation.constraints.NotBlank;
 
 public record Link(
         @NotBlank
+        @URL
         String link,
         String id
 ) {
