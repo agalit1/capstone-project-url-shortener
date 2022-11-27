6b291140044ff3243c919d7683e5b2ce59cf0290
anagalit <ana.galit1@gmail.com>
2022-11-27 15:43:26 +0100

    Add RedirectController for redirecting user to a website using the short url

--- backend/src/main/java/shortify/backend/model/LinkResponseDTO.java
+++ backend/src/main/java/shortify/backend/model/LinkResponseDTO.java
@@ -1,7 +1,15 @@
 package shortify.backend.model;
 
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.NotBlank;
+
 public record LinkResponseDTO(
+        @NotBlank
+        @URL
         String longLink,
+        @NotBlank
+        @URL
         String shortLink
 ) {
 }
