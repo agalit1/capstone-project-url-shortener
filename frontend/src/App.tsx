6b0ee1c3cd078dc4809b406e044ea0b680606c8b
anagalit <ana.galit1@gmail.com>
2022-11-27 13:19:41 +0100

    Install Material UI, add styling to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,34 @@
 import './Styling/App.css';
 import LinkCard from "./LinkCard";
 import {Route, BrowserRouter, Routes} from "react-router-dom";
+import {CssBaseline, Container, Typography, AppBar, Toolbar, Box} from "@mui/material";
+import {Link} from "@mui/icons-material";
 
 function App() {
 
     return (
-        <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<LinkCard/>}/>
-                </Routes>
-            </BrowserRouter>
-        </div>
+        <>
+            <CssBaseline/>
+            <Box sx={{flexGrow: 1}}>
+                <AppBar position="relative">
+                    <Toolbar>
+                        <Link/>
+                        <Typography variant="h6" component="div">
+                            It's Shortify time
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+            <Container>
+                <div>
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path='/' element={<LinkCard/>}/>
+                        </Routes>
+                    </BrowserRouter>
+                </div>
+            </Container>
+        </>
     )
 }
 
