991c400501a8f8ca150f330ab2c86df2ef942b8e
anagalit <ana.galit1@gmail.com>
2022-11-29 09:55:26 +0100

    Add integration test for RedirectController

--- backend/src/test/java/shortify/backend/LinkIntegrationTest.java
+++ backend/src/test/java/shortify/backend/LinkIntegrationTest.java
@@ -1,14 +1,17 @@
 package shortify.backend;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import shortify.backend.model.LinkResponseDTO;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -17,12 +20,35 @@ public class LinkIntegrationTest {
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @DirtiesContext
     @Test
     void getLinksAndExpectEmptyListOfLinks() throws Exception {
-        
+
         mvc.perform(MockMvcRequestBuilders.get("/api/links"))
                 .andExpect(status().isOk())
                 .andExpect((content().json("[]")));
+    }
+
+    @Test
+    void postLongLinkByIdAndExpectRedirectionUsingShortLink() throws Exception {
+
+        String body = mvc.perform(MockMvcRequestBuilders.post("/api/links")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(""" 
+                                    {
+                                        "link": "https://docs.oracle.com/javase/tutorial/java/nutsandbolts/while.html"
+                                    }
+                                """))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        LinkResponseDTO linkResponseDTO = objectMapper.readValue(body, LinkResponseDTO.class);
+
+        mvc.perform(MockMvcRequestBuilders.get("/" + linkResponseDTO.shortLink().split("/")[1]))
+                .andExpect(status().is(302))
+                .andExpect(redirectedUrl("https://docs.oracle.com/javase/tutorial/java/nutsandbolts/while.html"));
     }
 }
