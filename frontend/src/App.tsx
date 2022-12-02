54618634ed998d5c43f713028cf1e4fea0fe6b5f
anagalit <ana.galit1@gmail.com>
2022-12-02 16:22:13 +0100

    Add styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import LinkCard from "./LinkCard";
 import {Route, BrowserRouter, Routes} from "react-router-dom";
 import {CssBaseline, Container, Typography, AppBar, Toolbar, Box, Grid} from "@mui/material";
 import {Link} from "@mui/icons-material";
+import BackgroundAnimate from "./BackgroundAnimate";
 
 function App() {
 
@@ -12,10 +13,10 @@ function App() {
         <>
             <CssBaseline/>
             <Box sx={{flexGrow: 1, mb: 3}}>
-                <AppBar position="relative">
+                <AppBar position="relative" sx={{bgcolor: "black"}}>
                     <Toolbar>
-                        <Link/>
-                        <Typography variant="h6" component="div">
+                        <Link sx={{color: "#BB86FC", mt: 0.5, mb: 0.2, marginLeft: 3, marginRight: 0.5}}/>
+                        <Typography variant="h6" component="div" fontFamily='Lato'>
                             Shortify
                         </Typography>
                     </Toolbar>
@@ -24,9 +25,11 @@ function App() {
             <main>
                 <Box>
                     <Container maxWidth="sm">
-                        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
-                            URL Shortener
+                        <Typography display="inline" variant="h2" align="center" color="textPrimary" gutterBottom
+                                    fontFamily='Lato'>
+                            URL <span className="title">Shortener</span>
                         </Typography>
+                        <BackgroundAnimate/>
                         <Box>
                             <Grid container spacing={2}>
                                 <Grid item>
