03f78f3ac3064591452bca8e81a15418fd23b783
anagalit <ana.galit1@gmail.com>
2022-12-02 17:10:55 +0100

    Add styling to frontpage

---
+++ frontend/src/LinkResult.js
@@ -0,0 +1,9 @@
+import {useState} from "react";
+
+const LinkResult = () => {
+    const [shortenLink, setShortenLink] = useState()
+    return (
+        <div>LinkResult</div>
+    )
+}
+export default LinkResult;
