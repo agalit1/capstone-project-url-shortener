ca2d574620a489dc94d418b030792d56aefd24ee
anagalit <ana.galit1@gmail.com>
2022-12-05 13:35:15 +0100

    Add more styling to frontend

--- frontend/src/BackgroundAnimate.tsx
+++ frontend/src/BackgroundAnimate.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const backgroundAnimate = () => {
-    return (<ul className="background">
-        </ul>
+    return (
+        <div className="image">
+        </div>
     )
 }
 export default backgroundAnimate;
