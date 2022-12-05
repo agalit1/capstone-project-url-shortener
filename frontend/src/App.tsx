85aae81529f48943894c8dd8ead84e4b53df0ba9
anagalit <ana.galit1@gmail.com>
2022-12-05 13:35:15 +0100

    Add more styling to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import './style/App.css';
 import './style/style';
 import LinkCard from "./LinkCard";
-import {Route, BrowserRouter, Routes} from "react-router-dom";
-import {CssBaseline, Typography, AppBar, Toolbar, Box} from "@mui/material";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {AppBar, Box, CssBaseline, Toolbar, Typography} from "@mui/material";
 import {Link} from "@mui/icons-material";
 import BackgroundAnimate from "./BackgroundAnimate";
 import LinkResult from "./LinkResult";
@@ -30,14 +30,15 @@ function App() {
                 <Typography display="inline" variant="h2" align="center" color="textPrimary" gutterBottom
                             fontFamily='Lato'>
                     URL <span className="title">Shortener</span>
+                    <p className="slogan">Shortify - the shorter the better.</p>
                 </Typography>
-                <BackgroundAnimate/>
                 <BrowserRouter>
                     <Routes>
                         <Route path='/' element={<LinkCard setShortLink={setShortLink}/>}/>
                     </Routes>
                 </BrowserRouter>
                 <LinkResult shortLink={shortLink}/>
+                <BackgroundAnimate/>
             </div>
         </>
     )
