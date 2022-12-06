2969c723e652e4cc1d731fc73d516db187d40de9
anagalit <ana.galit1@gmail.com>
2022-12-06 16:01:00 +0100

    Add SignUp component in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import './style/App.css';
 import './style/style';
-import LinkCard from "./LinkCard";
+import LinkCard from "./Components/Link/LinkCard";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {AppBar, Box, CssBaseline, Toolbar, Typography} from "@mui/material";
+import {AppBar, Box, Button, CssBaseline, Toolbar, Typography} from "@mui/material";
 import {Link} from "@mui/icons-material";
-import BackgroundAnimate from "./BackgroundAnimate";
-import LinkResult from "./LinkResult";
+import BackgroundAnimate from "./Components/Link/BackgroundAnimate";
+import LinkResult from "./Components/Link/LinkResult";
 import {useState} from "react";
+import SignUpCard from "./Components/User/SignUpCard";
 
 
 function App() {
@@ -17,12 +18,16 @@ function App() {
         <>
             <CssBaseline/>
             <Box sx={{flexGrow: 1, mb: 3}}>
-                <AppBar position="relative" sx={{bgcolor: "black"}}>
+                <AppBar position="relative" sx={{bgcolor: "white"}}>
                     <Toolbar>
                         <Link sx={{color: "#BB86FC", mt: 0.5, mb: 0.2, marginLeft: 3, marginRight: 0.5}}/>
                         <Typography variant="h6" component="div" fontFamily='Lato'>
                             Shortify
                         </Typography>
+                        <div className="signup">
+                            <Button color="inherit">Sign Up</Button>
+                            <SignUpCard/>
+                        </div>
                     </Toolbar>
                 </AppBar>
             </Box>
