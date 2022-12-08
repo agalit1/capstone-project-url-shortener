ff37350762d6f2a804d5654296c2c72d9568d4d2
anagalit <ana.galit1@gmail.com>
2022-12-08 21:53:08 +0100

    Add user messages for sign up

--- frontend/src/Components/User/SignUpCard.tsx
+++ frontend/src/Components/User/SignUpCard.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Avatar, Box, Button, Container, CssBaseline, Grid, Link, TextField,} from "@mui/material";
 import LockPersonOutlinedIcon from '@mui/icons-material/LockPersonOutlined';
 import axios from "axios";
+import * as yup from 'yup';
 import {InferType, object, string} from 'yup';
 import {yupResolver} from "@hookform/resolvers/yup";
 import {useForm} from "react-hook-form";
@@ -18,19 +19,26 @@ const signUpSchema = object({
         .min(8, "Must be at least 8 characters")
         .required("Password is required"),
     confirmPassword: string()
-        .oneOf(Object.values("password"), "Password must match")
-        // .oneOf([Yup.ref("password"), null], "Password must match")
+        .oneOf([yup.ref("password"), null], "Password must match")
         .required("Confirm password is required")
 })
-
-function onSubmit(values: Props) {
-    return axios.post("/api/users/signup", values);
-
-}
 
 type Props = InferType<typeof signUpSchema>;
 
 function SignUpCard() {
+
+    const [success, setSuccess] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+
+    function onSubmit(values: Props) {
+        axios.post("/api/users/signup", values)
+            .then((response) => {
+                setSuccess(true)
+            })
+            .catch(() => {
+                setErrorMessage("")
+            })
+    }
 
     const {
         register,
@@ -43,25 +51,33 @@ function SignUpCard() {
     return (
         <Container component="main" maxWidth="xs">
             <CssBaseline/>
-            <Box
-                sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center',
-                }}
-            >
-                <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
-                    <LockPersonOutlinedIcon/>
-                </Avatar>
-                <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{mt: 3}} autoComplete="off">
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <TextField
-                                {...register("username")}
-                                required
-                                fullWidth
-                                id="username"
+            {success ? (
+                <section>
+                    <h1>Registration successful</h1>
+                    <p>
+                        <a href="#">Sign in</a>
+                    </p>
+                </section>
+            ) : (
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{m: 1, bgcolor: '#BB86FC'}}>
+                        <LockPersonOutlinedIcon/>
+                    </Avatar>
+                    <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{mt: 3}} autoComplete="off">
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    {...register("username")}
+                                    required
+                                    fullWidth
+                                    id="username"
                                 label="Username"
                                 autoFocus
                             />
@@ -98,25 +114,27 @@ function SignUpCard() {
                                 id="confirmPassword"
                             />
                         </Grid>
-                        <span className="error">{errors?.confirmPassword?.message}</span>
-                    </Grid>
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        sx={{mt: 3, mb: 2}}
-                    >
-                        Sign Up
-                    </Button>
-                    <Grid container justifyContent="flex-end">
-                        <Grid item>
-                            <Link href="#" variant="body2">
-                                Already have an account? Sign in
-                            </Link>
+                            <span className="error">{errors?.confirmPassword?.message}</span>
                         </Grid>
-                    </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{mt: 3, mb: 2}}
+                        >
+                            Sign Up
+                        </Button>
+                        <Grid container justifyContent="flex-end">
+                            <Grid item>
+                                {/*routerlink*/}
+                                <Link href="#" variant="body2" sx={{color: "#BB86FC"}}>
+                                    Already have an account? Sign in
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
                 </Box>
-            </Box>
+            )}
         </Container>
     );
 }
